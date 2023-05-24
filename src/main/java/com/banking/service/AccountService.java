433a4218a305d1de45145613587d61dd9dfeec2f
vikivero <vikichan.ch@gmail.com>
2023-05-24 17:18:32 +0200

    adds AccountService

---
+++ src/main/java/com/banking/service/AccountService.java
@@ -0,0 +1,11 @@
+package com.banking.service;
+
+import com.banking.entity.Account;
+
+public class AccountService {
+    public Account getByAccountNumber(long accountNumber) {
+        //TODO:refactor stub
+        return new Account(accountNumber,"jfidsfln");
+
+    }
+}
